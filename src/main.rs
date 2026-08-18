09b81c229de8d804c2ce7095767607277378fbf7
Siddharth Narayan <siddharth.060605@gmail.com>
2026-08-18 00:50:44 -0400

    Font scanning wasn't enabled, gonna crash out

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,56 @@
+use std::io::{Error, ErrorKind};
+
 use typst::{Library, LibraryExt, diag::FileResult, foundations::{Bytes, Datetime, Duration}, syntax::{FileId, RootedPath, Source, VirtualPath, VirtualRoot}, text::{Font, FontBook}, utils::LazyHash};
-use typst_kit::{files::{FileStore, FsRoot, SystemFiles}, fonts::FontStore, packages};
+use typst_kit::{files::{FileLoader, FileStore, FsRoot, SystemFiles}, fonts::FontStore, packages::SystemPackages};
+use typst_layout::PagedDocument;
+use typst_svg::SvgOptions;
 
 fn main() {
+
+    let world = World::new();
+
+    let result =  typst::compile(&world);
+    let wanings  = result.warnings;
+
+    println!("{:#?}", wanings);
+    let document: PagedDocument = result.output.unwrap();
+
+    println!("dpc{:#?}", document.pages()[0]);
+
+    let out = typst_svg::svg(&document.pages()[0], &SvgOptions { render_bleed: false, pretty: true });
+    println!("{}", out)
 }
 
 pub struct World {
     lib: LazyHash<Library>,
     fonts: FontStore,
     main: FileId,
+    files: FileStore<SystemFiles>
 }
 
 impl World {
     pub fn new() -> World {
+        let files = FileStore::new(SystemFiles::new(FsRoot::new(".".into()), SystemPackages::new(Downloader)));
+
         let vroot = VirtualRoot::Project;
-        let vpath = VirtualPath::new(".").unwrap();
+        let vpath = VirtualPath::new("test.typ").unwrap();
         let root = RootedPath::new(vroot, vpath);
+        let main = FileId::new(root);
 
-        // let fonts = typst_assets::fonts().flat_map(|d| Font::iter(Bytes::new(d))).collect();
-        // let font_book = FontBook::from_fonts(&fonts).into();
-        let fonts = FontStore::default();
+        println!("{:#?}", main);
 
+        let mut fonts = FontStore::new();
+        fonts.extend(typst_kit::fonts::system());
+        
         World { 
             lib: Library::builder().build().into(),
             fonts,
-            main: FileId::new(root)
+            main,
+            files
         }
     }
 }
+
 
 impl typst::World for World {
     fn library(&self) ->  &LazyHash<Library>  {
@@ -42,7 +66,10 @@ impl typst::World for World {
     }
 
     fn source(&self, id: FileId) -> FileResult<Source>  {
-        panic!("nah")
+        println!("id: {:#?}", id);
+        let a  =self.files.source(id);
+        println!("{:#?}", a.clone().unwrap().text());
+        a
     }
 
     fn file(&self, id: FileId) -> FileResult<Bytes>  {
@@ -50,10 +77,25 @@ impl typst::World for World {
     }
 
     fn font(&self, index: usize) -> Option<Font>  {
-        self.fonts.font(index)
+        let a = self.fonts.font(index);
+        println!("font: {:#?}", a);
+
+        a
     }
 
     fn today(&self, offset: Option<Duration>) -> Option<Datetime>  {
         Datetime::from_ymd(1970, 1, 1)
+    }
+}
+
+struct Downloader;
+impl typst_kit::downloader::Downloader for Downloader {
+    fn stream(
+        &self,
+        key: &dyn std::any::Any,
+        url: &str,
+    ) -> std::io::Result<(Option<usize>, Box<dyn std::io::prelude::Read>)>
+    {
+        std::io::Result::Err(Error::from(ErrorKind::AddrInUse))
     }
 }
---
+++ test.typ
@@ -0,0 +1,9 @@
+#set page(fill: color.rgb(255, 0, 0))
+== Hello
+abcd
+ba
+
+abcdaewg
+
+
+1

170c7bb0daed207ebe6f0f216547f343f4678e86
Siddharth Narayan <siddharth.060605@gmail.com>
2026-08-18 00:09:46 -0500

    Cleanup + Nixify

---
+++ flake.nix
@@ -0,0 +1,97 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    fenix.url = "github:nix-community/fenix";
+  };
+
+  outputs =
+    {
+      self,
+      flake-utils,
+      naersk,
+      nixpkgs,
+      fenix,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = (import nixpkgs) {
+          inherit system;
+          overlays = [ fenix.overlays.default ];
+        };
+
+        # toolchain = (pkgs.fenix.fromManifestFile ./rust-toolchain.toml);
+        toolchain = pkgs.fenix.complete.toolchain;
+        
+        naersk' = naersk.lib.${system}.override {
+          cargo = toolchain;
+          rustc = toolchain;
+        };
+
+        naerskBuildPackage =
+          target: args: naersk'.buildPackage (cargoConfig // args // { CARGO_BUILD_TARGET = target; });
+
+        # All of the CARGO_* configurations which should be used for all
+        # targets.
+        #
+        # Only use this for options which should be universally applied or which
+        # can be applied to a specific target triple.
+        #
+        # This is also merged into the devShell.
+        cargoConfig = {
+          src = ./.;
+          copyLibs = true;
+
+          # Tells Cargo to enable static compilation.
+          # (https://doc.rust-lang.org/cargo/reference/config.html#targettriplerustflags)
+          #
+          # Note that the resulting binary might still be considered dynamically
+          # linked by ldd, but that's just because the binary might have
+          # position-independent-execution enabled.
+          # (see: https://github.com/rust-lang/rust/issues/79624#issuecomment-737415388)
+          CARGO_TARGET_X86_64_UNKNOWN_LINUX_MUSL_RUSTFLAGS = "-C target-feature=+crt-static";
+
+          # Tells Cargo that it should use Wine to run tests.
+          # (https://doc.rust-lang.org/cargo/reference/config.html#targettriplerunner)
+          CARGO_TARGET_X86_64_PC_WINDOWS_GNU_RUNNER = pkgs.writeScript "wine-wrapper" ''
+            export WINEPREFIX="$(mktemp -d)"
+            exec wine64 $@
+          '';
+        };
+      in
+      rec {
+        packages.default = packages.linux;
+
+        packages.linux = naerskBuildPackage "x86_64-unknown-linux-gnu" {};
+
+        packages.windows = naerskBuildPackage "x86_64-pc-windows-gnu" {
+          depsBuildBuild = with pkgs; [
+            pkgsCross.mingwW64.stdenv.cc
+            pkgsCross.mingwW64.windows.pthreads
+          ];
+
+          nativeBuildInputs = with pkgs; [
+            # We need Wine to run tests:
+            wineWowPackages.stable
+          ];
+        };
+
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            jdk25
+            pkg-config
+            clang
+
+            rustPlatform.bindgenHook
+
+            openssl
+
+            alejandra
+            toolchain
+          ];
+        };
+      }
+    );
+}
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,34 @@
 use std::io::{Error, ErrorKind};
 
-use typst::{Library, LibraryExt, diag::FileResult, foundations::{Bytes, Datetime, Duration}, syntax::{FileId, RootedPath, Source, VirtualPath, VirtualRoot}, text::{Font, FontBook}, utils::LazyHash};
-use typst_kit::{files::{FileLoader, FileStore, FsRoot, SystemFiles}, fonts::FontStore, packages::SystemPackages};
+use typst::{
+    Library, LibraryExt,
+    diag::FileResult,
+    foundations::{Bytes, Datetime, Duration},
+    syntax::{FileId, RootedPath, Source, VirtualPath, VirtualRoot},
+    text::{Font, FontBook},
+    utils::LazyHash,
+};
+use typst_kit::{
+    files::{FileStore, FsRoot, SystemFiles},
+    fonts::FontStore,
+    packages::SystemPackages,
+};
 use typst_layout::PagedDocument;
 use typst_svg::SvgOptions;
 
 fn main() {
-
     let world = World::new();
 
-    let result =  typst::compile(&world);
-    let wanings  = result.warnings;
-
-    println!("{:#?}", wanings);
+    let result = typst::compile(&world);
     let document: PagedDocument = result.output.unwrap();
 
-    println!("dpc{:#?}", document.pages()[0]);
-
-    let out = typst_svg::svg(&document.pages()[0], &SvgOptions { render_bleed: false, pretty: true });
+    let out = typst_svg::svg(
+        &document.pages()[0],
+        &SvgOptions {
+            render_bleed: false,
+            pretty: true,
+        },
+    );
     println!("{}", out)
 }
 
@@ -25,39 +36,37 @@ pub struct World {
     lib: LazyHash<Library>,
     fonts: FontStore,
     main: FileId,
-    files: FileStore<SystemFiles>
+    file_store: FileStore<SystemFiles>,
 }
 
 impl World {
     pub fn new() -> World {
-        let files = FileStore::new(SystemFiles::new(FsRoot::new(".".into()), SystemPackages::new(Downloader)));
+        let file_store = FileStore::new(SystemFiles::new(
+            FsRoot::new(".".into()),
+            SystemPackages::new(Downloader),
+        ));
 
-        let vroot = VirtualRoot::Project;
-        let vpath = VirtualPath::new("test.typ").unwrap();
-        let root = RootedPath::new(vroot, vpath);
-        let main = FileId::new(root);
-
-        println!("{:#?}", main);
+        let main_path = VirtualPath::new("test.typ").unwrap();
+        let main = FileId::new(RootedPath::new(VirtualRoot::Project, main_path));
 
         let mut fonts = FontStore::new();
         fonts.extend(typst_kit::fonts::system());
-        
-        World { 
+
+        World {
             lib: Library::builder().build().into(),
             fonts,
             main,
-            files
+            file_store,
         }
     }
 }
 
-
 impl typst::World for World {
-    fn library(&self) ->  &LazyHash<Library>  {
+    fn library(&self) -> &LazyHash<Library> {
         &self.lib
     }
 
-    fn book(&self) ->  &LazyHash<FontBook>  {
+    fn book(&self) -> &LazyHash<FontBook> {
         self.fonts.book()
     }
 
@@ -65,25 +74,19 @@ impl typst::World for World {
         self.main
     }
 
-    fn source(&self, id: FileId) -> FileResult<Source>  {
-        println!("id: {:#?}", id);
-        let a  =self.files.source(id);
-        println!("{:#?}", a.clone().unwrap().text());
-        a
+    fn source(&self, id: FileId) -> FileResult<Source> {
+        self.file_store.source(id)
     }
 
-    fn file(&self, id: FileId) -> FileResult<Bytes>  {
+    fn file(&self, id: FileId) -> FileResult<Bytes> {
         todo!()
     }
 
-    fn font(&self, index: usize) -> Option<Font>  {
-        let a = self.fonts.font(index);
-        println!("font: {:#?}", a);
-
-        a
+    fn font(&self, index: usize) -> Option<Font> {
+        self.fonts.font(index)
     }
 
-    fn today(&self, offset: Option<Duration>) -> Option<Datetime>  {
+    fn today(&self, offset: Option<Duration>) -> Option<Datetime> {
         Datetime::from_ymd(1970, 1, 1)
     }
 }
@@ -94,8 +97,7 @@ impl typst_kit::downloader::Downloader for Downloader {
         &self,
         key: &dyn std::any::Any,
         url: &str,
-    ) -> std::io::Result<(Option<usize>, Box<dyn std::io::prelude::Read>)>
-    {
+    ) -> std::io::Result<(Option<usize>, Box<dyn std::io::prelude::Read>)> {
         std::io::Result::Err(Error::from(ErrorKind::AddrInUse))
     }
 }
